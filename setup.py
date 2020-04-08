4f58ef9c1b59823780c80fd3f4ffd5bec5110216
Ivan Bezerra Allaman <ivanalaman@gmail.com>
2020-04-08 20:03:56 -0300

    adiciona os primeiros arquivos do projeto
    Esses são os primeiros arquivos do projeto de exemplo do programa
    AceleraDev de D5.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="hello",
+    version="0.0.1",
+    description="Meu pacote",
+    author="Ivan Bezerra",
+    author_email="ivanalaman@gmail.com",
+    packages=["hello"]
+    #install_requires=["numpy"]
+)
